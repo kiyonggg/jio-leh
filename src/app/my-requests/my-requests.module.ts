739fa61262fc26bb6ace6251023d29fb40f23519
Ang Ki Yong <angkiyong@gmail.com>
2021-12-15 17:05:24 +0800

    bug fix - cannot create new request

--- src/app/my-requests/my-requests.module.ts
+++ src/app/my-requests/my-requests.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -15,6 +15,7 @@ import { RequestCardComponent } from '../request-card/request-card.component';
     CommonModule,
     FormsModule,
     IonicModule,
+    ReactiveFormsModule,
     MyRequestsPageRoutingModule
   ],
   declarations: [MyRequestsPage, CreateRequestModalComponent, RequestCardComponent]
